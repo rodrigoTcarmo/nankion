4f3ea00ed682cff980302dc68ac1d49e04620347
Rodrigo Carmo <rodrigo.carmo@nubank.com.br>
2024-08-18 14:30:00 -0300

    Create first database endpoint files

---
+++ pkg/api/databases/databases.go
@@ -0,0 +1,12 @@
+package databases
+
+import "github.com/gin-gonic/gin"
+
+const (
+	databaseId = "databaseId"
+	missingDatabaseIdErrorMessage = "Missing database ID!"
+)
+
+func GetDatabase(ctx *gin.Context){
+
+}
