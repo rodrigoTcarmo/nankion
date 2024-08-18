dbebd1ea6eb01f1c911d2ccfd4081dd967aafd2e
rodrigoTcarmo <rodrigoblock@outlook.com>
2024-08-18 17:23:20 -0300

    Refactor Notion client

--- pkg/notion/client.go
+++ pkg/notion/client.go
@@ -1,10 +1,23 @@
 package notion
 
 import (
+	"fmt"
 	"os"
+
 	"github.com/dstotijn/go-notion"
 )
 
-func NotionClient() *notion.Client{
-	return notion.NewClient(os.Getenv("NOTION_CARMO_SECRET"))
+type NankionClient struct {
+	Client *notion.Client
+}
+
+func NewNankionClient() *NankionClient{
+	secret := os.Getenv("NOTION_CARMO_SECRET")
+	if secret == ""{
+		fmt.Errorf("notion secret not found!")
+		return nil
+	}
+	return &NankionClient{
+		Client: notion.NewClient(secret),
+	}
 }
