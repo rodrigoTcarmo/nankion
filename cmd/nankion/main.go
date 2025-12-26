3b6e9fedd495c3533dbce8c4c147a869257495a2
Rodrigo Carmo <rodrigo.carmo@nubank.com.br>
2025-12-26 14:47:08 -0300

    Revert test for loop code

--- cmd/nankion/main.go
+++ cmd/nankion/main.go
@@ -74,10 +74,7 @@ func main() {
 				fmt.Printf("Error fetching database properties: %s\n", err)
 				return
 			}	
-			for _, property := range database {
-				fmt.Println(property.Name)
-				fmt.Println(property.Type)
-			}
+			fmt.Println("Database properties\n", database)
 		},
 	}
 
